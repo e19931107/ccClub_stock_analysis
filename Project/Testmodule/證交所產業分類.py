cff1e22182ef6ee9b100176bf0af48ca0cb68210
pchunlai <154031492+pchunlai@users.noreply.github.com>
2023-12-21 00:47:56 +0800

    新增證交所產業分類.py，但還沒跟主要的code併在一起

---
+++ Project/Testmodule/證交所產業分類.py
@@ -0,0 +1,28 @@
+def industry():
+    import requests 
+    import pandas as pd 
+    from bs4 import BeautifulSoup
+
+    url = "https://isin.twse.com.tw/isin/C_public.jsp?strMode=2" 
+    res = requests.get(url)
+
+    soup = BeautifulSoup(res.text, "lxml") 
+    tr = soup.findAll('tr')
+
+    tds = []
+    for raw in tr:
+        data = [td.get_text() for td in raw.findAll("td")]
+        if len(data) == 7:
+            tds.append(data)
+
+    df = pd.DataFrame(tds[1:],columns=tds[0])
+
+    df[['公司代號', '公司名稱']] = df['有價證券代號及名稱 '].str.split('　', expand=True)
+
+    # 刪除多餘欄位
+    columns_to_drop = ['有價證券代號及名稱 ', '國際證券辨識號碼(ISIN Code)', '上市日', '市場別', 'CFICode', '備註']
+    df = df.drop(columns=columns_to_drop)
+
+    df["Key_industry"] = df["公司代號"].astype(str) + df["公司名稱"].astype(str)
+    
+    return df
