5634441444f7aee7d05f51ac897edfd82b82e155
Edward <50692450+e19931107@users.noreply.github.com>
2023-12-21 10:29:04 +0800

    Delete some columns
    I used 公司代號 as a key to merge to another table.
    Therefore, key_industry is not necessary.
    Same for 公司名稱，already exist in the another table
    
    I merged industry and major table and move the code to up.
    
    Co-Authored-By: pchunlai <154031492+pchunlai@users.noreply.github.com>

--- Project/Testmodule/證交所產業分類.py
+++ Project/Testmodule/證交所產業分類.py
@@ -20,9 +20,9 @@ def industry():
     df[['公司代號', '公司名稱']] = df['有價證券代號及名稱 '].str.split('　', expand=True)
 
     # 刪除多餘欄位
-    columns_to_drop = ['有價證券代號及名稱 ', '國際證券辨識號碼(ISIN Code)', '上市日', '市場別', 'CFICode', '備註']
+    columns_to_drop = ['有價證券代號及名稱 ', '國際證券辨識號碼(ISIN Code)', '上市日', '市場別', 'CFICode', '備註', '公司名稱']
     df = df.drop(columns=columns_to_drop)
 
-    df["Key_industry"] = df["公司代號"].astype(str) + df["公司名稱"].astype(str)
+    # df["Key_industry"] = df["公司代號"].astype(str) + df["公司名稱"].astype(str)
     
     return df
