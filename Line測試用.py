c353e66a029f87cac613008b5f9c2debaa70bfef
Hsiao Jou Samantha Yu <samantha19980119@gmail.com>
2024-01-02 21:46:09 +0800

    Update Line測試用.py
    更換line token

--- Line測試用.py
+++ Line測試用.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, abort
 
 from linebot import (
@@ -13,14 +14,18 @@ from linebot.models import *
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('IZsto4Q2DbF+Z8AI8rbZpIdJD3F2Gwj49GhfSAH8dSux8IbdDnzZjSwKdrh3H408Qp/+D5Qyud13dDoMFtHiUGLC2iDfO2EjwX7l6+4hno0YkTrADTCdzFTeAUE/P8gNlmTkc6qG+mWrQabF9M4KUAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(
+    '+6ierndjKCQOOQ3BoF3WwiGhgkhUsLx77uI8wOK6ntAHVHUmDuG8RXBXFmv6U4tzLSwl0Zr2+e/TpfrqRjtVtyeMsTtBgfaOnSAOL5xrsUNABB0bqkmSHwa7tOoHfEtxodjt/7puwwp0nQy3gcl1GwdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('fbaae0bc1da0740e63b02f6cee11055c')
+handler = WebhookHandler('4f216dc67b4ec0e55a3cb9d175ee1c38')
 
 # 以下放入個人ID
-line_bot_api.push_message('Uf7c0d2712c0be085df517184ae2548ac', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message(
+    'U1770a65eee08d14a9e5e97e52da41022', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -39,15 +44,15 @@ def callback():
     return 'OK'
 
 
-#訊息傳遞區塊
+# 訊息傳遞區塊
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    line_bot_api.reply_message(event.reply_token, message)
 
-#主程式
-import os
+
+# 主程式
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
